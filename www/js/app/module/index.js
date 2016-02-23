5b038a5386877276b7c94ebff5a007adb4375471
minskyb <minskyb@163.com>
2016-02-23 17:35:16 +0800

    frame v1.0.0

--- www/js/app/module/index.js
+++ www/js/app/module/index.js
@@ -4,7 +4,27 @@
 'use strict';
 
 requirejs(['./common'],function(){
-    requirejs(['jquery','BModule'],function($,BM){
+    requirejs(['jquery','BModule','app/component/header','extra'],function($,BM,cHeader){
+
+        function Index(options){
+
+            BM.call(this);
+        };
+
+        Index.prototype = $.YK_OFFICIAL.inheritPrototype(BM.prototype);
+
+        Index.prototype.initProperty = function(){
+
+            BM.prototype.initProperty.call(this);
+            this.views = {
+                '.js-c-header':cHeader
+            }
+        }
+
+       $(document).ready(function(){
+           var index = new Index();
+           index.render();
+       });
 
     });
 })
