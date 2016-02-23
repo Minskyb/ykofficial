e2b094adf741342fd5ea8075910058e79cde86c2
minskyb <minskyb@163.com>
2016-02-23 17:22:13 +0800

    grunt muliti project object-oriented programming v1.0.0

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,6 +10,10 @@ module.exports = function(grunt){
         banner:'/**\n' +
         '* <%=pkg.name%> v <%=pkg.version%>'+
         '*/',
+        clean:{
+            build:'www_build',
+            css:'www/css'
+        },
         requirejs:{
             options:{
                 banner:'<%=banner%>\n'
@@ -17,10 +21,77 @@ module.exports = function(grunt){
             multi:{
                 options:{
                     appDir:'www',
-                    mainConfigFile:''
+                    mainConfigFile:'www/js/app/module/common.js',
+                    dir:'www_build',
+                    modules:[
+                        {
+                            name:'../app/module/common',
+                            include:[
+                                'jquery',
+                                'BView',
+                                'BModule',
+                                'text',
+                                'extra'
+                            ]
+                        },
+                        {
+                            name:'../app/module/index',
+                            include:[
+                                'app/component/header'
+                            ],
+                            exclude:[
+                                '../app/module/common'
+                            ]
+                        }
+                    ]
                 }
+            }
+        },
+        less:{
+            compileCore: {
+                options: {
+                    strictMath: true,
+                    sourceMap: true,
+                    outputSourceFiles: true,
+                    sourceMapURL: '<%= pkg.name %>.css.map',
+                    sourceMapFilename: 'www/css/main.css.map'
+                },
+                src: 'www/less/<%= pkg.name %>.less',
+                dest: 'www/css/main.css'
+            },
+            compileTheme: {
+                options: {
+                    strictMath: true,
+                    sourceMap: true,
+                    outputSourceFiles: true,
+                    sourceMapURL: '<%= pkg.name %>-theme.css.map',
+                    sourceMapFilename: 'www/css/main-theme.css.map'
+                },
+                src: 'www/less/theme.less',
+                dest: 'www/css/main-theme.css'
+            }
+        },
+        watch: {
+            less: {
+                files: 'www/less/**/*.less',
+                tasks: ['clean:css','less']
             }
         }
     });
+
+    require('load-grunt-tasks')(grunt,{ scope: 'devDependencies' });
+
+    grunt.registerTask('default',[
+        'clean:css',
+        'less',
+        'watch'
+    ]);
+
+    grunt.registerTask('build',[
+        'clean',
+        'less:build',
+        'less',
+        'requirejs'
+    ])
 
 }
--- www/js/app/module/abstract.module.js
+++ www/js/app/module/abstract.module.js
@@ -3,10 +3,51 @@
  */
 'use strict';
 
-define(['underscore','jquery','BView'],function(_,$,BV){
+define(['underscore','jquery'],function(_,$){
 
-    function BModule(option){
+    function BModule(options){
 
+        this.initProperty();
     }
 
-})
+    BModule.prototype.initProperty = function(){
+
+        // 事件监听配置
+        this.events = {};
+
+        // 组件配置
+        this.views = {}
+
+        // 组件集合
+        this.components = [];
+    }
+
+    BModule.prototype.initComponents = function(){
+
+        var that = this;
+        $.each(this.views,function(K,V){
+
+            if(K && 'function'== typeof V){
+                that.components.push(new V({$wrapper:$(K,that.$element)}));
+            }
+            else if(K && 'object' == typeof V){
+                var options = $.extend(true,{$wrapper:$(K,that.$element)}, V.option);
+                that.components.push(new V.Func(options));
+            }
+            else {
+                console.error("initComponents 组件配置错误！");
+            }
+        })
+    }
+
+    BModule.prototype.render = function(){
+
+        this.initComponents();
+        this.components.forEach(function(item){
+            item.render();
+        });
+    }
+
+
+    return BModule;
+});
