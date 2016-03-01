e6f6c4821f4f6efbac5c890ff6d89433597d3bb9
minskyb <minskyb@163.com>
2016-03-01 13:52:32 +0800

    前面忘记添加 home_team_thumbnail 模块了，现在补上

---
+++ www/js/app/component/home_team_thumbnail.js
@@ -0,0 +1,52 @@
+/**
+ * Created by ASUS on 2016/2/29.
+ */
+'use strict';
+
+define([
+    'jquery',
+    'BView',
+    'text!app/component/home_team_thumbnail.html',
+    'dataService',
+    'extra',
+    'modal'
+],function($,BV,template,dataService){
+
+    function HTeamThumbnail(options){
+
+        BV.call(this,options);
+    }
+
+    HTeamThumbnail.prototype = $.YK_OFFICIAL.inheritPrototype(BV.prototype);
+
+    HTeamThumbnail.prototype.initProperty = function(){
+
+        BV.prototype.initProperty.call(this);
+
+        this.template = template;
+
+    }
+
+    HTeamThumbnail.prototype.initData = function(){
+
+        var that = this;
+        $.ajax({
+            url:dataService.getPicList,
+            type:'post',
+            data:{type:3},
+            beforeSend:function(xhr){
+                //xhr.setRequestHeader('version',dataService.version);
+            },
+            success:function(res){
+
+                that.data ={
+                    "thumbnails":res.piclist
+                }
+
+                $.YK_OFFICIAL.create(that);
+            }
+        });
+    }
+
+    return HTeamThumbnail;
+});
